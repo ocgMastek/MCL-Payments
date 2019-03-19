c724e1fccee5b628552f795308aaf522cdce8369
Oliver <44974906+ocgMastek@users.noreply.github.com>
2019-03-19 14:28:41 +0000

    Add files via upload

---
+++ paymentprototype/src/main/java/com/mastek/paymentprototype/PaymentprototypeApplication.java
@@ -0,0 +1,13 @@
+package com.mastek.paymentprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentprototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentprototypeApplication.class, args);
+	}
+
+}
---
+++ paymentprototype/src/test/java/com/mastek/paymentprototype/PaymentprototypeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mastek.paymentprototype;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaymentprototypeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
